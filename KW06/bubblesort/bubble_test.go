db3f41bd844478efd17d6f7eeb1f8c5ad2507ab3
carp533 <olexyz@gmx.net>
2025-02-02 11:51:10 +0100

    KW06

--- KW06/bubblesort/bubble_test.go
+++ KW06/bubblesort/bubble_test.go
@@ -4,19 +4,17 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
-	"time"
 )
 
-func ExampleSort() {
+func ExampleBubbleSort() {
 	//Output:
 	//[3 8 11 14 17 18 43]
 	array := []int{11, 14, 3, 8, 18, 17, 43}
 	fmt.Println(BubbleSort(array))
 }
-func BenchmarkSort(b *testing.B) {
+func BenchmarkBubbleSort(b *testing.B) {
 	size := 100000
 	slice := make([]int, size)
-	rand.Seed(time.Now().UnixNano())
 	for i := 0; i < size; i++ {
 		slice[i] = rand.Intn(999) - rand.Intn(999)
 	}
