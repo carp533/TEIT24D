a4e87c7fc40c1d88c6b93f14b3846ba327051fc1
carp533 <olexyz@gmx.net>
2025-05-22 12:16:51 +0200

    trie

--- KW20/trie/main.go
+++ KW20/trie/main.go
@@ -10,17 +10,17 @@ type TrieNode struct {
 	isEnd    bool
 }
 
+// Trie represents the Trie structure with a root node.
+type Trie struct {
+	root *TrieNode
+}
+
 // NewTrieNode creates a new TrieNode.
 func NewTrieNode() *TrieNode {
 	return &TrieNode{
 		children: make(map[rune]*TrieNode),
 		isEnd:    false,
 	}
-}
-
-// Trie represents the Trie structure with a root node.
-type Trie struct {
-	root *TrieNode
 }
 
 // NewTrie creates a new Trie.
@@ -32,32 +32,74 @@ func NewTrie() *Trie {
 
 // Insert adds a word to the Trie.
 func (t *Trie) Insert(word string) {
+	// word ="apple"
+	node := t.root
+	for _, char := range word {
+		_, exists := node.children[char]
+		if !exists {
+			node.children[char] = NewTrieNode()
+		}
+		node = node.children[char]
+		fmt.Printf("%v-", char)
+	}
+	fmt.Println()
 
+	node.isEnd = true
 }
 
 // Search checks if a word is in the Trie.
 func (t *Trie) Search(word string) bool {
-	return false
+	node := t.root
+	for _, char := range word {
+		if _, exists := node.children[char]; !exists {
+			return false
+		}
+		node = node.children[char]
+	}
+	return node.isEnd
 }
 
 // StartsWith checks if any word in the Trie starts with the given prefix.
 func (t *Trie) StartsWith(prefix string) bool {
-	return false
+	node := t.root
+	for _, char := range prefix {
+		if _, exists := node.children[char]; !exists {
+			return false
+		}
+		node = node.children[char]
+	}
+	return true
 }
 
 // GetWordsWithPrefix returns all words in the Trie that start with the given prefix.
 func (t *Trie) GetWordsWithPrefix(prefix string) []string {
-	return []string{}
+	result := make([]string, 0)
+	node := t.root
+	for _, char := range prefix {
+		if _, exists := node.children[char]; !exists {
+			return result
+		}
+		node = node.children[char]
+	}
+	t.collectWords(node, prefix, &result)
+	return result
 }
 
-// collectWords is a helper function to perform a search and collect words from the given node.
+// collectWords is a helper function to perform a search and collect
+// words from the given node.
 func (t *Trie) collectWords(node *TrieNode, currentWord string, words *[]string) {
-
+	if node.isEnd {
+		*words = append(*words, currentWord)
+	}
+	for char, childNode := range node.children {
+		t.collectWords(childNode, currentWord+string(char), words)
+	}
 }
 
 func main() {
 	trie := NewTrie()
-	words := []string{"apple", "app", "apricot", "banana", "band", "bandana", "cat", "car", "cart"}
+	words := []string{"apple", "app", "apricot", "banana",
+		"band", "bandana", "cat", "car", "cart"}
 	for _, word := range words {
 		trie.Insert(word)
 	}
